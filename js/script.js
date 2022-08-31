381aec2b52d77513dbdf6bd4721983341b4e531c
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-08-31 16:36:42 +0200

    palindroma in console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 console.log('JS OK!');
+
+
+
+// ----- PALINDROMA ----- 
+
+//chiedi al utente una parola
+
+const word = prompt('Enter a word: ');
+
+//invocare la funzione
+
+const value = checkForPalindrome(word);
+
+console.log(value);
+
+//vedere se la parola inserita e una palindroma
+
+function checkForPalindrome(word) {
+    let wordLength = word.length;
+
+    for (let i = 0; i < wordLength / 2; i++) {
+        if (word[i] !== word[wordLength - 1 - i]) {
+            return 'Your word is not a palindrome';
+        }
+        return 'Your word is a palindrome'
+    }
+}
+
+
