ec6e89331c67605fe5d20e3c04d5e041a6b893c7
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-09-01 10:16:45 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('JS OK!');
 
 
 // ----- PALINDROMA ----- 
-
+/*
 //chiedi al utente una parola
 
 const word = prompt('Enter a word: ');
@@ -26,5 +26,64 @@ function checkForPalindrome(word) {
         return 'Your word is a palindrome'
     }
 }
+
+*/
+
+const word = prompt('Enter a word: ');
+
+const isPalindroma = checkPalindrome(word);
+
+function checkPalindroma(text) {
+    console.log(text);
+    console.log(text.charAt[0]);
+
+    let reversedText = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text.charAt(i);
+
+        reversedText = char + reversedText;
+        console.log(reversedText);
+
+    }
+
+
+
+    // const isEqual = reversedText === text;
+    // return isEqual;
+
+
+    //versione 2
+    console.log('versione 2');
+    reversedText = '';
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        const element = array[i];
+
+        const char = text.charAt[i];
+        reversedText = reversedText + char;
+        console.log(reversedText);
+    }
+
+    //versione 3
+    console.log('versione 3');
+
+    const arrayText = text.split('');
+
+    const reversedArray = arrayText.reverse();
+
+    reversedText = reversedArray.join('');
+
+
+
+    console.log('reversedText', reversedText);
+
+    const isEqual = reversedText === text;
+
+    return isEqual;
+}
+
+
+
 
 
